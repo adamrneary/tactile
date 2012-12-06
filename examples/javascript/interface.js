e5bece9539fd044f7a4a6a985a52f37b0154e691
Zack Maril <zack@zacharymaril.com>
2012-12-06 10:19:15 -0800

    Target interface

---
+++ examples/javascript/interface.js
@@ -0,0 +1,55 @@
+//TODO: Figuring out what data comes from where. 
+
+var data = [];
+for (var i=0; i<20; i++){
+    var y = Math.random()*10;
+    data.push({x:i, yup: y , ydown: (10 -y) });
+}
+
+function get(prop){
+    return function(d){
+        return d[prop];
+    };
+}
+
+var barSeries = {
+    name: 'example',
+    color: 'blue',
+    renderer: 'bar',
+    wide: true,
+    data: barData,
+    xValue: get("x"),
+    yValue: get("y"),
+    tooltip: function(d){
+        return "On "+ d.y +" there were " + d.x + " guys online.";
+    },
+    afterDrag: function(d){
+        return console.log("bar dragged "+d);
+    }
+};
+
+var lineSeries = {
+    name: 'example',
+    color: 'red',
+    renderer: 'line',
+    data: dataSeries,
+    xValue: get("x"),
+    yValue: get("y"),
+    tooltip: function(d){
+        return "On "+ d.y +" there were " + d.x + " guys online.";
+    },
+    afterDrag: function(d){
+        return console.log("line dragged "+d);
+    }
+};
+
+var chart = new Tactile.Chart({
+      axes:{
+          x: "time",
+          y: "linear"
+      },
+      series:[barSeries,lineSeries],
+      timeFrame: [-5, 6],
+      data: [0]                            
+      });
+chart.render();
