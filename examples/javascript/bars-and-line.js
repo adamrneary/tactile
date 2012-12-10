5a7fdb86065f4cd42d9103c87e5b50da397560ea
Zack Maril <zack@zacharymaril.com>
2012-12-10 09:45:13 -0800

    Non identity transform

--- examples/javascript/bars-and-line.js
+++ examples/javascript/bars-and-line.js
@@ -10,6 +10,12 @@ var chart = new Tactile.Chart({
                                                 console.log(d);
                                                 console.log(draggedSeries);}}],
                                   data: [ {x:0, y:5}, {x:1, y:1}, {x:2,y:5}, {x:3, y:7} ],
+                                  dataTransform: function(d){
+                                      return {
+                                          x:d.x,
+                                          y:(d.y+1)
+                                      };
+                                  },
                                   element: $("#example_view")[0]
                               });
 chart.render();
