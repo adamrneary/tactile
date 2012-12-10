013ec0c170973877848c62e11d320b30489e6e19
Zack Maril <zack@zacharymaril.com>
2012-12-09 23:02:06 -0800

    Cleaning up code for examples.

--- examples/javascript/index.js
+++ examples/javascript/index.js
@@ -1,21 +1,33 @@
-(function(){
-var examples = ["simple-line","bars-and-line","stacked-bars"];
-
-var showcaseObject = {routes:{}};
-
-_.map(examples,function(route){
-          showcaseObject.routes[route] = route;
-          showcaseObject[route] = function(){
-              $.get("javascript/"+route+".js",function(data){
-                        $("#example_js").html(data);                        
-                        eval(data);//TODO Find a better way to do this
-                        });
-          };          
-});
-
-var Showcase = Backbone.Router.extend(showcaseObject);
-var showcase = new Showcase();
 $(document).ready(function(){
+                      var examples = [
+                          {shortLink:"simple-line", title: "Simple Line"},
+                          {shortLink:"bars-and-line", title: "Bars and Line"},
+                          {shortLink:"stacked-bars", title: "Stacked Bars"}
+                      ];
+
+
+
+                      var showcaseObject = {routes:{}};
+                      var linkList = $("#linkList");
+
+                      _.map(examples,function(route){
+                                console.log(route.shortLink,route.title);
+                                var link = $("<a>").attr("href","/#"+route.shortLink).text(route.title);
+                                var li = $("<li>").append(link);
+                                linkList.append(li);
+                                console.log(link,li);
+
+                                showcaseObject.routes[route.shortLink] = route.shortLink;
+                                showcaseObject[route.shortLink] = function(){
+                                    $("#example_view").html("");
+                                    $.get("javascript/"+route.shortLink+".js",function(data){
+                                              $("#example_js").html(data);
+                                          });
+                                };          
+                            });
+
+                      var Showcase = Backbone.Router.extend(showcaseObject);
+                      var showcase = new Showcase();
                       Backbone.history.start();
+                      showcase.navigate("/#simple-line");
                   });
-})();
--- examples/javascript/simple-line.js
+++ examples/javascript/simple-line.js
@@ -1,6 +1,8 @@
 var chart = new Tactile.Chart({
                                   series: [{renderer: "line"}],
                                   data: [{x:0, y:0},{x:1, y:1},{x:2,y:2}],
-                                  element: $("#example_view")[0]
+                                  element: $("#example_view")[0],
+                                  height: 400,
+                                  width: 500
                               });
 chart.render();                      
