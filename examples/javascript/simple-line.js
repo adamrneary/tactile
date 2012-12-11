104cc46809cf834ca386503708d57262cba7dc35
Kamil Sarna <kamil.sarna@gmail.com>
2012-12-11 16:34:08 +0100

    Format example the same way as bars-and-line for readability

--- examples/javascript/simple-line.js
+++ examples/javascript/simple-line.js
@@ -1,8 +1,14 @@
 var chart = new Tactile.Chart({
-                                  series: [{renderer: "line"}],
-                                  data: [{x:0, y:200},{x:1, y:1},{x:2,y:200}],
-                                  element: $("#example_view")[0],
-                                  height: 400,
-                                  width: 500
-                              });
+    element: $("#example_view")[0],
+    width: 500,
+    height: 400,
+    data: [{x:0, y:200},{x:1, y:1},{x:2,y:200}],
+      
+    series: [
+      {
+        name: "simple line",
+        renderer: "line"
+      }
+    ]
+});
 chart.render();                      
