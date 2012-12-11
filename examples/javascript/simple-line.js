a9f4d31d094d89992a705539a4582b640a2772f6
Kamil Sarna <kamil.sarna@gmail.com>
2012-12-11 15:26:15 +0100

    Make line example more interesting

--- examples/javascript/simple-line.js
+++ examples/javascript/simple-line.js
@@ -1,6 +1,6 @@
 var chart = new Tactile.Chart({
                                   series: [{renderer: "line"}],
-                                  data: [{x:0, y:0},{x:1, y:1},{x:2,y:2}],
+                                  data: [{x:0, y:200},{x:1, y:1},{x:2,y:200}],
                                   element: $("#example_view")[0],
                                   height: 400,
                                   width: 500
