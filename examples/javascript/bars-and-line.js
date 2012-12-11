5374233cb5e288f80eae52bdeadec95bc5f9bb2d
Zack Maril <zack@zacharymaril.com>
2012-12-10 23:53:09 -0800

    Interface is being pinned down via readme and examples.

--- examples/javascript/bars-and-line.js
+++ examples/javascript/bars-and-line.js
@@ -5,17 +5,18 @@ var chart = new Tactile.Chart({
                                             wide: true,
                                             round: true},
                                            {renderer: "draggableLine",
+                                            dataTransform: function(d){
+                                                return {
+                                                    x:d.x,
+                                                    y:(d.y+1)
+                                                };
+                                            },
                                             afterDrag: function(d, draggedSeries){
                                                 console.log("Dragged:");
                                                 console.log(d);
-                                                console.log(draggedSeries);}}],
+                                                console.log(draggedSeries);
+                                            }}],
                                   data: [ {x:0, y:5}, {x:1, y:1}, {x:2,y:5}, {x:3, y:7} ],
-                                  dataTransform: function(d){
-                                      return {
-                                          x:d.x,
-                                          y:(d.y+1)
-                                      };
-                                  },
                                   element: $("#example_view")[0]
                               });
 chart.render();
--- readme.md
+++ readme.md
@@ -6,7 +6,17 @@ To view the examples:
 ``` bash
 npm install
 grunt build
-python -m SimpleHTTPServer
+foreman start -f Procfile
 ```
 
-Then navigate to `TODO:someplace with examples`
+Then navigate to the [showcase](http://localhost:5000). 
+
+# Data Philosophy 
+
+Key tenets of Tactile
+* A chart is based around one list of data. 
+* A chart can display as many series as needed. 
+* Each series takes the chart data and transforms it as needed via dataTransform. Must
+  return objects that are of the form `{x: NUMBER, y: NUMBER}`.
+* The chart determines the axes to use and timeframe with which to display
+  the data of the series. 
