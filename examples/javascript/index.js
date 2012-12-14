d1fb8b763b4cf7aa88e575dd9956c696bce70d6f
Adam Neary <adam.neary@profitably.com>
2012-12-14 10:04:02 -0500

    spliting up the nav menu a bit

--- examples/javascript/index.js
+++ examples/javascript/index.js
@@ -1,60 +1,111 @@
-var examples = [
-    {
-        shortLink: "line", 
-        title: "Line"
-    },
-    {
-        shortLink: "area", 
-        title: "Area"
-    },
-    {
-        shortLink:"sliding-timeframe", 
-        title: "Sliding timeframe"
-    },
-    {
-        shortLink: "multiple-series", 
-        title: "Multiple series"
-    },
-    {
-        shortLink: "gauge",
-        title: "Gauge"
-        
-    }
+var cartesian = [
+  {
+    shortLink: "line", 
+    title: "Line"
+  },
+  {
+    shortLink: "column", 
+    title: "Column"
+  },
+  {
+    shortLink: "area", 
+    title: "Area"
+  },
+  {
+    shortLink: "multiple-series", 
+    title: "Multiple series types"
+  }
+];
+
+var noncartesian = [
+  {
+    shortLink: "gauge",
+    title: "Gauge"
+  }
+];
+
+var components = [
+  {
+    shortLink:"sliding-timeframe", 
+    title: "Sliding timeframe"
+  }
 ];
 
 var showcaseObject = {
-    routes:{}
+  routes:{}
 };
 
-
 $(document).ready(function(){
-                      var linkList = $("#linkList");
-                      _.map(examples,function(route){
-                                var link = $("<a>")
-                                    .attr("href","/#"+route.shortLink)
-                                    .text(route.title);
-                                linkList.append($("<li>").append(link));
 
-                                showcaseObject.routes[route.shortLink] = route.shortLink;
-                                showcaseObject[route.shortLink] = function(){
-                                    $("#example_header").text(route.title);
-                                    var url = "javascript/"+route.shortLink+".js";
-                                    var script = $("<script>").attr("src",url);
-                                    $("#example_view").empty().append(script);
-                                    $("#example_js").load(url,function(){
-                                                              $(this).removeClass("rainbow");
-                                                              Rainbow.color();
-                                                              });
-                                    Rainbow.color();
-                                };          
-                            });
+  // TODO: remove the duplicative code i just created!
+  var linkList = $("#cartLinkList");
+  _.map(cartesian,function(route){
+    var link = $("<a>")
+      .attr("href","/#"+route.shortLink)
+      .text(route.title);
+    linkList.append($("<li>").append(link));
 
-                      var Showcase = Backbone.Router.extend(showcaseObject);
-                      var showcase = new Showcase();
+    showcaseObject.routes[route.shortLink] = route.shortLink;
+    showcaseObject[route.shortLink] = function(){
+      $("#example_header").text(route.title);
+      var url = "javascript/"+route.shortLink+".js";
+      var script = $("<script>").attr("src",url);
+      $("#example_view").empty().append(script);
+      $("#example_js").load(url,function(){
+        $(this).removeClass("rainbow");
+        Rainbow.color();
+      });
+      Rainbow.color();
+    };          
+  });
 
+  var linkList = $("#noncartLinkList");
+  _.map(noncartesian,function(route){
+    var link = $("<a>")
+      .attr("href","/#"+route.shortLink)
+      .text(route.title);
+    linkList.append($("<li>").append(link));
 
-                      Backbone.history.start();
-                      if(!window.location.hash) {
-                          showcase.navigate("/#line");
-                      }
-                  });
+    showcaseObject.routes[route.shortLink] = route.shortLink;
+    showcaseObject[route.shortLink] = function(){
+      $("#example_header").text(route.title);
+      var url = "javascript/"+route.shortLink+".js";
+      var script = $("<script>").attr("src",url);
+      $("#example_view").empty().append(script);
+      $("#example_js").load(url,function(){
+        $(this).removeClass("rainbow");
+        Rainbow.color();
+      });
+      Rainbow.color();
+    };          
+  });
+
+  var linkList = $("#compLinkList");
+  _.map(components,function(route){
+    var link = $("<a>")
+      .attr("href","/#"+route.shortLink)
+      .text(route.title);
+    linkList.append($("<li>").append(link));
+
+    showcaseObject.routes[route.shortLink] = route.shortLink;
+    showcaseObject[route.shortLink] = function(){
+      $("#example_header").text(route.title);
+      var url = "javascript/"+route.shortLink+".js";
+      var script = $("<script>").attr("src",url);
+      $("#example_view").empty().append(script);
+      $("#example_js").load(url,function(){
+        $(this).removeClass("rainbow");
+        Rainbow.color();
+      });
+      Rainbow.color();
+    };          
+  });
+
+  var Showcase = Backbone.Router.extend(showcaseObject);
+  var showcase = new Showcase();
+
+  Backbone.history.start();
+  if(!window.location.hash) {
+    showcase.navigate("/#line");
+  }
+});
