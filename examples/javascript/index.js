aed1f366eadeec1cf07da0d698f2e5a30d3dc1f7
Kamil Sarna <kamil.sarna@gmail.com>
2012-12-18 12:24:39 +0100

    Remove duplicated code from the index.js

--- examples/javascript/index.js
+++ examples/javascript/index.js
@@ -1,132 +1,99 @@
 var cartesian = [
-  {
-    shortLink: "scatter", 
-    title: "Scatter (future)"
-  },
-  {
-    shortLink: "line", 
-    title: "Line"
-  },
-  {
-    shortLink: "column", 
-    title: "Column"
-  },
-  {
-    shortLink: "stacked-column", 
-    title: "Stacked column"
-  },
-  {
-    shortLink: "area", 
-    title: "Area"
-  },
-  {
-    shortLink: "multiple-series", 
-    title: "Multiple series types"
-  }
+{
+  shortLink: "scatter", 
+  title: "Scatter (future)"
+},
+{
+  shortLink: "line", 
+  title: "Line"
+},
+{
+  shortLink: "column", 
+  title: "Column"
+},
+{
+  shortLink: "stacked-column", 
+  title: "Stacked column"
+},
+{
+  shortLink: "area", 
+  title: "Area"
+},
+{
+  shortLink: "multiple-series", 
+  title: "Multiple series types"
+}
 ];
 
 var noncartesian = [
-  {
-    shortLink: "gauge",
-    title: "Gauge"
-  },
-  {
-    shortLink: "leaderboard",
-    title: "Leaderboard (future)"
-  },
-  {
-    shortLink: "sankey",
-    title: "Sankey (future)"
-  },
-  {
-    shortLink: "multi-donut",
-    title: "Multiple donuts (future)"
-  }
+{
+  shortLink: "gauge",
+  title: "Gauge"
+},
+{
+  shortLink: "leaderboard",
+  title: "Leaderboard (future)"
+},
+{
+  shortLink: "sankey",
+  title: "Sankey (future)"
+},
+{
+  shortLink: "multi-donut",
+  title: "Multiple donuts (future)"
+}
 ];
 
 var components = [
-  {
-    shortLink:"sliding-timeframe", 
-    title: "Sliding timeframe"
-  },
-  {
-    shortLink:"filter", 
-    title: "Filters (future)"
-  },
-  {
-    shortLink:"legend", 
-    title: "Legend (future)"
-  }
+{
+  shortLink:"sliding-timeframe", 
+  title: "Sliding timeframe"
+},
+{
+  shortLink:"filter", 
+  title: "Filters (future)"
+},
+{
+  shortLink:"legend", 
+  title: "Legend (future)"
+}
 ];
 
 var showcaseObject = {
   routes:{}
 };
 
+
+var prepareLinks = function(route, el){
+  var link = $("<a>")
+  .attr("href","/#"+route.shortLink)
+  .text(route.title);
+  el.append($("<li>").append(link));
+  showcaseObject.routes[route.shortLink] = route.shortLink;
+  showcaseObject[route.shortLink] = function(){
+    $("#example_header").text(route.title);
+    var url = "javascript/"+route.shortLink+".js";
+    var script = $("<script>").attr("src",url);
+    $("#example_view").empty().append(script);
+    $("#example_js").load(url,function(){
+      $(this).removeClass("rainbow");
+      Rainbow.color();
+    });
+    Rainbow.color();
+  };          
+}
+
 $(document).ready(function(){
 
   // TODO: remove the duplicative code i just created!
-  var linkList = $("#cartLinkList");
-  _.map(cartesian,function(route){
-    var link = $("<a>")
-      .attr("href","/#"+route.shortLink)
-      .text(route.title);
-    linkList.append($("<li>").append(link));
-
-    showcaseObject.routes[route.shortLink] = route.shortLink;
-    showcaseObject[route.shortLink] = function(){
-      $("#example_header").text(route.title);
-      var url = "javascript/"+route.shortLink+".js";
-      var script = $("<script>").attr("src",url);
-      $("#example_view").empty().append(script);
-      $("#example_js").load(url,function(){
-        $(this).removeClass("rainbow");
-        Rainbow.color();
-      });
-      Rainbow.color();
-    };          
+  _.map(cartesian, function(route){
+    prepareLinks(route, $("#cartLinkList"));
   });
-
-  var linkList = $("#noncartLinkList");
-  _.map(noncartesian,function(route){
-    var link = $("<a>")
-      .attr("href","/#"+route.shortLink)
-      .text(route.title);
-    linkList.append($("<li>").append(link));
-
-    showcaseObject.routes[route.shortLink] = route.shortLink;
-    showcaseObject[route.shortLink] = function(){
-      $("#example_header").text(route.title);
-      var url = "javascript/"+route.shortLink+".js";
-      var script = $("<script>").attr("src",url);
-      $("#example_view").empty().append(script);
-      $("#example_js").load(url,function(){
-        $(this).removeClass("rainbow");
-        Rainbow.color();
-      });
-      Rainbow.color();
-    };          
+  _.map(cartesian, function(route){
+    prepareLinks(route, $("#noncartLinkList"));
   });
-
-  var linkList = $("#compLinkList");
-  _.map(components,function(route){
-    var link = $("<a>")
-      .attr("href","/#"+route.shortLink)
-      .text(route.title);
-    linkList.append($("<li>").append(link));
-
-    showcaseObject.routes[route.shortLink] = route.shortLink;
-    showcaseObject[route.shortLink] = function(){
-      $("#example_header").text(route.title);
-      var url = "javascript/"+route.shortLink+".js";
-      var script = $("<script>").attr("src",url);
-      $("#example_view").empty().append(script);
-      $("#example_js").load(url,function(){
-        $(this).removeClass("rainbow");
-        Rainbow.color();
-      });
-      Rainbow.color();
-    };          
+  _.map(cartesian, function(route){
+    prepareLinks(route, $("#compLinkList"));
   });
 
   var Showcase = Backbone.Router.extend(showcaseObject);
